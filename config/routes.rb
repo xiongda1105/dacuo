7c723470352c8535bd84c8bbb4ec7d3684c93023
vagrant <vagrant@localhost.localdomain>
2020-06-13 14:46:56 +0000

    Update toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/index'
-  get 'posts/show'
-  get 'homes/top'
+  root "homes#top"
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
