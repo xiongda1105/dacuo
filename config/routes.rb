2e46dc1ac1e8ca10646b334791ce4c6c8352905e
vagrant <vagrant@localhost.localdomain>
2020-06-13 14:41:34 +0000

    First

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/index'
+  get 'posts/show'
+  get 'homes/top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
